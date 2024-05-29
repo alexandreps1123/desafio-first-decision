8174fe6d6bb58afc47e4493c278f94bc5f649b0f
alexandreps1123 <alexandre.ps1123@gmail.com>
2024-05-28 21:27:24 -0300

    feat: variables to manage pagination and method to choose status style

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, inject } from '@angular/core';
 import { Users } from '../models/user.model';
 import { NgbModal, NgbModalConfig, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { UsersFormComponent } from './users-form/users-form.component';
-// import { PaginationInstance } from 'ngx-pagination';
+import { PaginationInstance } from 'ngx-pagination';
+import { Status } from '../enums/status.enum';
 
 @Component({
   selector: 'app-users',
@@ -11,40 +12,684 @@ import { UsersFormComponent } from './users-form/users-form.component';
 })
 export class UsersComponent implements OnInit {
 
-  // config: PaginationInstance = {
-  //   itemsPerPage: 20,
-  //   currentPage: 1
-  // };
-
   listaUsuarios: Users = [
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
     {
       nome: "Ran-na",
       sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
       dataCriacao: new Date(),
       ultimoAcesso: new Date(),
-    }
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Xan-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.ATIVO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.PENDENTE,
+      dataCriacao: new Date(),
+      ultimoAcesso: null,
+    },
+    {
+      nome: "Ran-na",
+      sobrenome: "Ran-na",
+      email: "aaa@aa.com",
+      status: Status.BLOQUEADO,
+      dataCriacao: new Date(),
+      ultimoAcesso: new Date(),
+    },
+    
   ];
 
+  itemsPerPage: number[] = [10, 20, 50];
+
+  config: PaginationInstance = {
+    itemsPerPage: 10,
+    currentPage: 1
+  };
+
+
+	filter: string = '';
   private modalService = inject(NgbModal);
 
-
-  constructor(config: NgbModalConfig) {}
+  constructor(config: NgbModalConfig) { }
 
   ngOnInit() {
 
-
-    // telefone?: string;
-    // email?: string;
-    // perfilAcesso?: string[];
-    // idioma?: number;
-    // tipoContato?: TipoContato;
-    // status?: Status;
-    // dataCriacao?: Date;
-    // ultimoAcesso?: Date;
-  }
+   }
 
   open(): NgbModalRef {
     return this.modalService.open(UsersFormComponent, { size: 'lg' });
   }
+  
+  getSeverity(status: string ): string {
+    if (status === Status.ATIVO) { return "success"; } 
+    else if (status === Status.PENDENTE) { return "warning"; }
+    else { return "danger"; }
+  }
+  
 }
 
